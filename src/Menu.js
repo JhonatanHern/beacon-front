5cd70d7dd143e467286a7bbc0f4d8ff873d11497
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-04-15 16:07:19 -0400

    main menu layout and css finished

--- src/Menu.js
+++ src/Menu.js
@@ -10,18 +10,34 @@ const getX = el => {
   }
   return left + width / 2 
 }
-
 class Menu extends Component{
+  state = {
+    x:0,
+    showMyPodcasts : false,
+  }
+  buttonRef =  React.createRef()
+  toggleNavAndUpdateX = display => {
+    this.setState({
+      showMyPodcasts : display,
+      x : getX(this.buttonRef.current)
+    })
+  }
   render(){
     return (
-      <header id="logged-header">
+      <header id="logged-header" onMouseLeave={e=>this.toggleNavAndUpdateX(false)}>
         <img alt="" src="/beacon.png"/>
         <div>
           <Link to="/">Home</Link>
-          <button>My Podcast</button>
+          <button ref={this.buttonRef} onMouseEnter={e=>this.toggleNavAndUpdateX(true)}>My Podcast</button>
           <Link to="">Add Episode</Link>
         </div>
         <Link to="/profile">Profile</Link>
+        <nav className="my-podcasts" style={{left:this.state.x,maxHeight:this.state.showMyPodcasts?"20em":'0'}}>
+          <Link to="/my-podcast/1">My podcast #1</Link>
+          <Link to="/my-podcast/1">My podcast #2</Link>
+          <Link to="/my-podcast/1">My podcast #3</Link>
+          <Link to="/my-podcast/1">My podcast #4</Link>
+        </nav>
       </header>
     )
   }
